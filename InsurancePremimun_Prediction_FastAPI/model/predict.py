d7556c58ed09fbf1c9367acb46254b11f35daa78
Preet <112680439+cipher-preet@users.noreply.github.com>
2025-06-24 22:42:36 +0530

    Project wth Fast API and Docker Integration

---
+++ InsurancePremimun_Prediction_FastAPI/model/predict.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import pickle
+
+
+with open('model/model.pkl','rb') as f:
+    model = pickle.load(f)
+
+
+# get class labels from model (important for matching prabablities to class names)
+class_labels = model.classes_.tolist()
+
+
+def predict_output(user_input: dict):
+
+    df = pd.DataFrame([user_input])
+     # Predict the class
+    predicted_class = model.predict(df)[0]
+
+    # Get probabilities for all classes
+    probabilities = model.predict_proba(df)[0]
+    confidence = max(probabilities)
+    
+    # Create mapping: {class_name: probability}
+    class_probs = dict(zip(class_labels, map(lambda p: round(p, 4), probabilities)))
+
+    return {
+        "predicted_category": predicted_class,
+        "confidence": round(confidence, 4),
+        "class_probabilities": class_probs
+    }
+
